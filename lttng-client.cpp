5f2e2b59f1bed79367889fb73651ede6c78b6b0e
Umit <umit@fsl.cs.sunysb.edu>
2019-06-30 17:05:07 -0400

    enabling mmap read tracepoints from user space

--- lttng-client.cpp
+++ lttng-client.cpp
@@ -104,6 +104,9 @@ void lttng_config(void) {
       "sudo lttng enable-event -s strace2ds-session -c channel0 --kernel "
       "mm_filemap_add_to_page_cache >> lttng-client.log");
   system(
+      "sudo lttng enable-event -s strace2ds-session -c channel0 --kernel "
+      "mm_filemap_fsl_read >> lttng-client.log");
+  system(
       "sudo lttng add-context -k --session=strace2ds-session --type=tid >> "
       "lttng-client.log");
   system(
