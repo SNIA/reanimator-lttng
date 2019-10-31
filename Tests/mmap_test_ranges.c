206c1587332e0dae3b317cd87522bc151bd005a5
Umit <umit@fsl.cs.sunysb.edu>
2019-10-31 19:24:58 -0400

    mmap test & makefiles

---
+++ Tests/mmap_test_ranges.c
@@ -0,0 +1,58 @@
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+
+#define handle_error(msg) \
+  do {                    \
+    perror(msg);          \
+    exit(EXIT_FAILURE);   \
+  } while (0)
+
+int main(){
+    char *memblock;
+    int fd;
+    struct stat sb;
+
+    fd = open("mmaptest.file", O_RDWR, 0);
+
+    fstat(fd, &sb);
+    printf("file desc: %d, size: %lu\n", fd, (uint64_t)sb.st_size);
+
+    printf("Original content:\n");
+    memblock = mmap(NULL, 4096 * 10, O_RDWR, MAP_SHARED, fd, 0);
+    if (memblock == MAP_FAILED) handle_error("mmap");
+
+    for (uint64_t i = 0; i < 10; i++) {
+        printf("[%lu]=%X ", i, memblock[i]);
+    }
+    printf("\n");
+
+    //unmap the beginning
+    int ret = munmap(memblock, 4096);
+    printf("Unmapped from the beginning, success? %d\n", ret);
+
+    //right overlap
+    ret = munmap(memblock, 4096 * 2);
+    printf("Right overlap, success? %d\n", ret);
+
+    //left overlap
+    ret = munmap(memblock + 4096 * 9, 4096);
+    printf("Left overlap, success? %d\n", ret);
+
+    //enclosed
+    ret = munmap(memblock + 4096 * 3, 4096 * 5);
+    printf("Enclosed overlap, success? %d\n", ret);
+
+    //enclosing
+    ret = munmap(memblock, 4096 * 4);
+    printf("Enclosing overlap, success? %d\n", ret);
+
+    close(fd);
+    return 0;
+}
