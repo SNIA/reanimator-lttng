5f3a46c7b953f18d305d8303e9cb4c926823e8d8
lvkv <velikovlukas@gmail.com>
2020-08-04 22:22:43 -0400

    Add config args, number of cores, all-at-once apt install

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 #
-# Install script for fsl-lttng and dependencies
+# Description: Install script for fsl-lttng and dependencies
+# Authors:     Thomas Fleming, Lukas Velikov
 
 ####################
 # Script variables #
 ####################
-# TODO: See if numberOfCores is necessary
-# TODO: See if appending configArgs is necessary
-# readonly numberOfCores="$(nproc --all)"
-# configArgs=""
-readonly dependencies=("autoconf" "libtool" "make" "gcc" "g++" "perl" "git" "flex" "bison" "asciidoc" "libpopt-dev" "libxml2-dev" "uuid-dev" "libgtk2.0-dev" "libelf-dev" "libdw-dev")
+readonly programDependencies=("autoconf" "libtool" "make" "gcc" "g++" "perl" "git" "flex" "bison" "asciidoc" "libpopt-dev" "libxml2-dev" "uuid-dev" "libgtk2.0-dev" "libelf-dev" "libdw-dev")
+readonly numberOfCores="$(nproc --all)"
+configArgs=""
 install=false
-installPackages=false
 installDir="$(pwd)/fsl-lttng"
+installPackages=false
+missingPrograms=()
 repositoryDir="$(pwd)/build"
 
 ####################
 # Script functions #
 ####################
+
+# Wrapper function for running commands with feedback
 function runcmd
 {
     echo "CMD: $*"
@@ -29,6 +31,7 @@ function runcmd
     fi
 }
 
+# Output usage string (invoked on -h or --help)
 function printUsage
 {
     (
@@ -52,6 +55,11 @@ while [[ $# -gt 0 ]]; do
     key="$1"
 
     case "${key}" in
+        --config-args)
+            shift # past argument
+            "${configArgs}"="$1"
+            shift # past value
+            ;;
         --install)
             install=true
             installDir="/usr/local"
@@ -77,7 +85,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 # See if sudo is installed
-if [[ "${install}" = true ]]; then
+if [[ "${install}" == true ]]; then
     if ! command -v sudo &>/dev/null; then
         echo "Script could not find 'sudo' command. Cannot install." >&2
         exit 1
@@ -85,58 +93,71 @@ if [[ "${install}" = true ]]; then
 fi
 
 # Check whether program dependencies are installed
-for program in "${dependencies[@]}"; do
+for program in "${programDependencies[@]}"; do
     programPath=$(command -v "${program}")
     if [[ $? == 0 ]]; then
         echo "${program}: Located at ${programPath}"
     elif [[ "${installPackages}" == true ]]; then
-        echo "${program}: Not found. Installing..."
-        runcmd sudo apt-get install -y "${program}"
+        echo "${program}: Not found. Queuing for installation..."
+        missingPrograms+=("${program}")
     else
         echo "${program}: Not found."
         missingPrograms+=("${program}")
     fi
 done
 
+# Check whether the user has all required programs for building
+if [[ "${#missingPrograms[@]}" -gt 0 ]]; then
+    if [[ "${installPackages}" == true ]]; then
+        echo "Installing missing programs."
+        sudo apt-get install -y "${missingPrograms[*]}"
+    else
+        echo "Could not find all required programs. Not found:"
+        for program in "${missingPrograms[@]}"; do
+            echo "  ${program}"
+        done
+
+        echo "To install on a Debian-based system, run the command"
+        echo "  sudo apt-get install ${missingPrograms[*]}"
+        exit 1
+    fi
+fi
+
 # Clone repositories
-# TODO: Change SSH git clone to HTTPS
 runcmd mkdir -p "${repositoryDir}"
 runcmd cd "${repositoryDir}"
-[[ -d "userspace-rcu" ]] || runcmd git clone git@github.com:sbu-fsl/userspace-rcu.git
-[[ -d "lttng-ust" ]] || runcmd git clone git@github.com:sbu-fsl/lttng-ust.git
-[[ -d "lttng-tools" ]] || runcmd git clone git@github.com:sbu-fsl/lttng-tools.git
-[[ -d "lttng-modules" ]] || runcmd git clone git@github.com:sbu-fsl/lttng-modules.git
-[[ -d "babeltrace" ]] || runcmd git clone git@github.com:sbu-fsl/babeltrace.git
-[[ -d "trace2model" ]] || runcmd git clone git@github.com:sbu-fsl/trace2model.git
-[[ -d "fsl-strace" ]] || runcmd git clone git@github.com:sbu-fsl/fsl-strace.git
-# Commented out for now, since we're putting this script in fsl-lttng
-# [[ -d "fsl-lttng" ]] || runcmd git clone git@github.com:sbu-fsl/fsl-lttng.git
-
+[[ -d "userspace-rcu" ]] || runcmd git clone https://github.com/sbu-fsl/userspace-rcu.git
+[[ -d "lttng-ust" ]] || runcmd git clone https://github.com/sbu-fsl/lttng-ust.git
+[[ -d "lttng-tools" ]] || runcmd git clone https://github.com/sbu-fsl/lttng-tools.git
+[[ -d "lttng-modules" ]] || runcmd git clone https://github.com/sbu-fsl/lttng-modules.git
+[[ -d "babeltrace" ]] || runcmd git clone https://github.com/sbu-fsl/babeltrace.git
+[[ -d "trace2model" ]] || runcmd git clone https://github.com/sbu-fsl/trace2model.git
+[[ -d "fsl-strace" ]] || runcmd git clone https://github.com/sbu-fsl/fsl-strace.git
 
 # Install userspace-rcu
 runcmd cd userspace-rcu
 runcmd ./bootstrap
-runcmd ./configure
-runcmd make
-runcmd sudo make install
+runcmd ./configure "${configArgs}"
+runcmd make -j"${numberOfCores}" 
+runcmd sudo make -j"${numberOfCores}" install
 runcmd sudo ldconfig
 runcmd cd "${repositoryDir}"
 
 # Install lttng-ust
 runcmd cd  lttng-ust
 runcmd ./bootstrap
-runcmd ./configure
-runcmd make
-runcmd sudo make install
+runcmd ./configure "${configArgs}"
+runcmd make -j"${numberOfCores}" 
+runcmd sudo make -j"${numberOfCores}" install
 runcmd sudo ldconfig
 runcmd cd "${repositoryDir}"
 
 # Install lttng-tools
 runcmd cd lttng-tools
 runcmd ./bootstrap
-runcmd ./configure
-rumcmd make
-runcmd sudo make install
+runcmd ./configure "${configArgs}"
+rumcmd make -j"${numberOfCores}" 
+runcmd sudo make -j"${numberOfCores}" install
 runcmd sudo ldconfig
 runcmd cd "${repositoryDir}"
 
@@ -144,8 +165,8 @@ runcmd cd "${repositoryDir}"
 # Exhausts memory at 1 GiB memory, so try to have more
 # Requires fsl-lttng-linux kernel
 runcmd cd lttng-modules
-runcmd make
-runcmd sudo make modules_install
+runcmd make -j"${numberOfCores}" 
+runcmd sudo make -j"${numberOfCores}" modules_install
 runcmd sudo depmod -a
 runcmd cd "${repositoryDir}"
 
@@ -153,15 +174,14 @@ runcmd cd "${repositoryDir}"
 runcmd cd babeltrace
 runcmd git checkout master  # Default branch depends on strace2ds
 runcmd ./bootstrap
-runcmd ./configure
-runcmd make
-runcmd sudo make install
+runcmd ./configure "${configArgs}"
+runcmd make -j"${numberOfCores}" 
+runcmd sudo make install -j"${numberOfCores}" 
 runcmd sudo ldconfig
 runcmd cd "${repositoryDir}"
 
 # Install fsl-strace
 runcmd cd fsl-strace
-runcmd git checkout buildall_script  # TODO: Remove when buildall script gets merged into master
 runcmd sudo chmod +x dist/build-syscall-replayer.sh
 runcmd sudo dist/build-syscall-replayer.sh
 runcmd cd "${repositoryDir}"
