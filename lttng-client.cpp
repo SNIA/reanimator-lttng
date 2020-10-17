9f2e41e2fad26d60e01624d9735a20fc3d3ef440
lvkv <velikovlukas@gmail.com>
2020-10-17 19:29:23 -0400

    Add copyright to top of every *.cpp, hpp, c, h file

--- lttng-client.cpp
+++ lttng-client.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019 Erez Zadok
+ * Copyright (c) 2019-2020 Ibrahim Umit Akgun
+ * Copyright (c) 2020 Lukas Velikov */
+
 // Copyright FSL Lab Stony Brook University
 
 #include <stdlib.h>
